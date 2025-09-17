c4bbd70924c743ae1c1477cc6d3043f8fc058a2b
azzazhr <azzahraqina70@gmail.com>
2025-09-17 19:22:13 +0700

    add jobsheet 3 - OOP dengan Studi                  Kasus Dino Jump

--- Jobsheet 3/praktikum_dino_jump/main.dart
+++ Jobsheet 3/praktikum_dino_jump/main.dart
@@ -1,14 +1,12 @@
 import 'dino.dart';
 import 'cactus.dart';
 import 'game_object.dart';
-import 'bird.dart';
 
 void main(){
   var dino = Dino(0, 0);
   var cactus = Cactus(10, 0);
-  var bird = Bird(15, 5);
 
-  List<GameObject> gameObjects = [dino, cactus, bird];
+  List<GameObject> gameObjects = [dino, cactus];
 
   for (var obj in gameObjects){
     obj.update();
