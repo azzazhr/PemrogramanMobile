6cec4daa4b1464dbb3625847b36487b0a0d91e28
azzazhr <azzahraqina70@gmail.com>
2025-09-17 19:29:58 +0700

    add TugasJobsheet 3 - No2

--- Jobsheet 3/praktikum_transkrip_nilai/main.dart
+++ Jobsheet 3/praktikum_transkrip_nilai/main.dart
@@ -1,4 +1,5 @@
 import 'mata_kuliah.dart';
+import 'dart:io';
 
 void main(){
   List<MataKuliah> transkrip = [
@@ -7,20 +8,37 @@ void main(){
     MataKuliah('IF103', 'Pemrograman', 4, 3.0),
   ];
 
-  double totalNilai = transkrip
-    .map((mk) => mk.nilai * mk.sks)
-    .reduce((a, b) => a + b);
+  while (true) {
+    print('\n=== Menu Transkrip Mahasiswa ===');
+    print('1. Hitung IPK');
+    print('2. Filter Nilai Bagus (>= 3.5)');
+    print('3. Tampilkan Nilai Terformat');
+    print('0. Keluar');
+    stdout.write('Pilih opsi: ');
+    var input = stdin.readLineSync();
 
-  int totalSks = transkrip.map((mk) => mk.sks).reduce((a,b) => a + b);
-  double ipk = totalNilai / totalSks;
-  print('IPK: ${ipk.toStringAsFixed(2)}');
-
-  var nilaiBagus = transkrip.where((mk) => mk.nilai >= 3.5);
-  print('\nMata kuliah dengan nilai bagus:');
-  for (var mk in nilaiBagus) {
-    print('${mk.nama} (${mk.nilai})');
+    if (input == '1') {
+      double totalNilai = transkrip
+          .map((mk) => mk.nilai * mk.sks)
+          .reduce((a, b) => a + b);
+      int totalSks =
+      transkrip.map((mk) => mk.sks).reduce((a, b) => a + b);
+      double ipk = totalNilai / totalSks;
+      print('IPK: ${ipk.toStringAsFixed(2)}');
+    } else if (input == '2') {
+      var nilaiBagus = transkrip.where((mk) => mk.nilai >= 3.5);
+      print('\nMata kuliah dengan nilai bagus:');
+      for (var mk in nilaiBagus) {
+        print('${mk.nama} (${mk.nilai})');
+      }
+    } else if (input == '3') {
+      print('\nDaftar Ringkas:');
+      transkrip.map((mk) => '${mk.nama}: ${mk.nilai}').forEach(print);
+    } else if (input == '0') {
+      print('Keluar program...');
+      break;
+    } else {
+      print('Pilihan tidak valid!');
+    }
   }
-
-  print('\nDaftar Ringkas:');
-  transkrip.map((mk) => '${mk.nama} : ${mk.nilai}').forEach(print);
 }
