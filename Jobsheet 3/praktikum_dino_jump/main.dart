d28f7a07d3404e441d94ebfd569601800dfc1849
azzazhr <azzahraqina70@gmail.com>
2025-09-17 19:28:56 +0700

    add TugasJobsheet 3 - No1

--- Jobsheet 3/praktikum_dino_jump/main.dart
+++ Jobsheet 3/praktikum_dino_jump/main.dart
@@ -1,12 +1,14 @@
 import 'dino.dart';
 import 'cactus.dart';
 import 'game_object.dart';
+import 'bird.dart';
 
 void main(){
   var dino = Dino(0, 0);
   var cactus = Cactus(10, 0);
+  var bird = Bird(15, 5);
 
-  List<GameObject> gameObjects = [dino, cactus];
+  List<GameObject> gameObjects = [dino, cactus, bird];
 
   for (var obj in gameObjects){
     obj.update();
